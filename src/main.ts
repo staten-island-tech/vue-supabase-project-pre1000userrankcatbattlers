01a83c66a7331be99768c4cd9c737c5223652249
TLCChen <tianlangchen0@gmail.com>
2024-04-05 13:22:23 -0400

    4-5-24

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import "./assets/main.css";
+import './assets/main.css'
 
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-const app = createApp(App);
+import App from './App.vue'
+import router from './router'
 
-app.use(createPinia());
+const app = createApp(App)
 
-app.mount("#app");
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/TianlangchenView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ingredients">
+    <h1></h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.ingredients {
+  width: 30vw;
+  height: 10vh;
+
+  position: absolute;
+  left: 0px;
+  bottom: 0px;
+
+  background-color: red;
+  border-radius: 0% 10% 10% 0%;
+}
+</style>
