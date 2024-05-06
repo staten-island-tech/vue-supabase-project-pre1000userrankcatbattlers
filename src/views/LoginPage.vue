d9861493654e62a6ba028c0b07e9ef686f7502e1
Keasalte <112724822+Keasalte@users.noreply.github.com>
2024-05-06 13:14:54 -0400

    SSS
    Soap Secret Service

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,21 +1,28 @@
 <template>
-  <h1>Test Form</h1>
-  <div class="Input">
-    <label for="email">Email: </label>
-    <input type="email" id="email" />
-  </div>
-  <div class="Input">
-    <label for="email">Password: </label>
-    <input type="password" id="password" />
-  </div>
+  <div class="form">
+    <h1>Test Form</h1>
+    <div class="Input">
+      <label for="email">Email: </label>
+      <input type="email" id="email" />
+    </div>
+    <div class="Input">
+      <label for="email">Password: </label>
+      <input type="password" id="password" />
+    </div>
 
-  <div class="buttons">
-    <button>Register</button>
-    <button>Login</button>
+    <div class="buttons">
+      <button>Register</button>
+      <button>Login</button>
+    </div>
   </div>
 </template>
 
-<style>
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+}
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
