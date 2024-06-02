6ae69b2aeaa7fea37e3dee07b09e43052f6f2b19
TLCChen <139132998+TLCChen@users.noreply.github.com>
2024-06-02 11:37:45 -0400

    Update CreateRecipe.vue

--- src/views/CreateRecipe.vue
+++ src/views/CreateRecipe.vue
@@ -4,6 +4,12 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from "../../utils/supabase";
+import { ref, onMounted, type Ref } from "vue";
+import circleitem from "../components/circleitem.vue";
+import { router } from "@/router";
+import { RouterLink } from "vue-router";
+</script>
 
 <style scoped></style>
